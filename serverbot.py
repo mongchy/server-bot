e8a91751318c25ad309a206cc66049cd6b121ce6
mongchy <58220309+mongchy@users.noreply.github.com>
2019-11-26 22:35:50 +0900

    Add files via upload

---
+++ serverbot.py
@@ -0,0 +1,11 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+            await message.channel.send("그 대는 정말로 안녕이라고 말하였기 때문에 평민 등급을 주지")
+
+
+client.run("NjA3MTQyNDAxODM2MDU2NTc5.Xdp-Aw.Rt_jPT6TcFjVNK9XSBlkUe_adrg")
