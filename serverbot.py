67cf86acd26170d70b56be8f09bc13677d74ce67
mongchy <58220309+mongchy@users.noreply.github.com>
2019-11-26 23:11:19 +0900

    Update serverbot.py

--- serverbot.py
+++ serverbot.py
@@ -9,5 +9,4 @@ async def on_message(message):
             await message.channel.send("그 대는 정말로 안녕이라고 말하였기 때문에 평민 등급을 주지")
                                        
                                     
-access_token = os.enviorn["BOT_TOKEN"]
-client.run(access_token)
+client.run("NjA3MTQyNDAxODM2MDU2NTc5.Xdp-Aw.Rt_jPT6TcFjVNK9XSBlkUe_adrg")
